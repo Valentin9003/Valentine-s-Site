53bc7e5c0d09cb1a526f71054a4ebf0726af212a
Valentin <valio_180@abv.bg>
2019-08-19 19:52:42 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { currentLanguage } from "../Valentin9003.github.io/Valentine-s-Site/blob/master/Language/ChangeLanguage.js";
+import { currentLanguage } from ".//valentin9003.github.io/Valentine-s-Site/Language/ChangeLanguage.js";
 
 
 
